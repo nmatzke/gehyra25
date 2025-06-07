22a2d287a8d46bb9b37ed505729736be020a862e
Nicholas J. Matzke <nickmatzke.ncse@gmail.com>
2025-06-08 11:30:52 +1200

    updated deathRate=e code, recompiling

---
+++ 03_M0+BD/phybears_DEC+B+e_M0_mr3/lnLs_tuple.R
@@ -0,0 +1,8 @@
+input_tuple = list()
+input_tuple$total_calctime_in_sec = 0.113
+input_tuple$iteration_number = 14
+input_tuple$Julia_sum_lq = -340.9891831357734
+input_tuple$rootstates_lnL = -10.50436242772955
+input_tuple$Julia_total_lnLs1 = -351.49354556350295
+input_tuple$bgb_lnL = -83.23219481369057
+input_tuple$total_loglikelihood = -83.23219481369057
---
+++ 03_M0+BD/phybears_DECj+B+e_M0_mr3/lnLs_tuple.R
@@ -0,0 +1,8 @@
+input_tuple = list()
+input_tuple$total_calctime_in_sec = 0.124
+input_tuple$iteration_number = 14
+input_tuple$Julia_sum_lq = -334.31642911219814
+input_tuple$rootstates_lnL = -9.437965408698862
+input_tuple$Julia_total_lnLs1 = -343.754394520897
+input_tuple$bgb_lnL = -77.0589802528828
+input_tuple$total_loglikelihood = -77.0589802528828
